f3c19dd908b4dcdd7535384bbccb1edc7d667fbc
jaafari-R <jaafariramadan@gmail.com>
2024-02-03 01:48:29 +0200

    Added localStorage save for users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { useEffect, useState } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, json } from "react-router-dom";
 
 import Home from './components/Home';
 import Catalog from './components/Catalog';
@@ -50,8 +50,27 @@ const MOVIE_COST = 3;
 
 function App() {
     const [currentUserIndex, setCurrentUserIndex] = useState(null);
-    const [users, setUsers] = useState(USERS);
+    const [users, setUsers] = useState([]);
     const [catalog, setCatalog] = useState([]);
+
+    useEffect(() => {
+        const initUsers = 
+            (localStorage.users && JSON.parse(localStorage.users))
+            || USERS;
+        setUsers(initUsers.map(user => {
+            return {...user, rentedMovies: new Set(user.rentedMovies)}
+        }));
+    }, []);
+
+    useEffect(() => {
+        if(users.length) {
+            localStorage.users = 
+                JSON.stringify(users.map(
+                    user => {
+                        return {...user, rentedMovies: [...user.rentedMovies]};
+                    }));
+        }
+    }, [users])
 
     function getCatalog() {
         if(currentUserIndex === null) {
@@ -66,6 +85,7 @@ function App() {
     }
     
     useEffect(() => {
+        console.log(currentUserIndex, users)
         if(currentUserIndex !== null) {
             setCatalog(getCatalog());
         }
